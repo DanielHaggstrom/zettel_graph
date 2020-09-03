a1ce0055293c74146dfe70ae0249f8cafb1510af
Daniel Häggström <daniel.hpf@hotmail.com>
2020-09-03 13:27:19 +0200

    Selecciona sólo notas de tipo 1

--- main.py
+++ main.py
@@ -5,35 +5,53 @@ from itertools import repeat
 
 # guardaremos un diccionario con el id de la nota como clave, y una lista de las notas a las que linkea como atributo.
 resultado = {}
-# ahoremos una lista de las notas para traducir sus id por sus posiciones
-lista_notas = []
 
-# buscamos toda la información útil en la carpeta json
+# adquirimos todas las notas en un diccionario id: [tipo, contenido json]
 path = "D:\\Coding\\PyCharm Workspace\\zettel_graph\\venv\\json"
+todas = {}
 for file in os.listdir(path):
     id = file[:-5]
-    lista_notas.append(id)
     # cargamos cada json
     with open(path + "\\" + file, encoding="utf8") as f:
         nota = json.load(f)
-    # solo nos interesan notas con contenido
-    if "body" not in nota.keys():
+    todas[id] = [nota["type_"], nota] # si aquí no salta error, todas las notas tienen campo type_
+
+# ahoremos una lista de las notas para traducir sus id por sus posiciones
+lista_notas = []
+
+# buscamos los links en las notas de tipo 1, e ignoramos links a notas de otros tipos
+path = "D:\\Coding\\PyCharm Workspace\\zettel_graph\\venv\\json"
+for nota in todas.keys():
+    tipo = todas[nota][0]
+    contenido = todas[nota][1]
+    # solo nos interesan notas de tipo 1
+    if tipo != 1: # parece ser la diferencia entre notas normales y cuadernos, etc.
         continue
-    texto = nota["body"]
+    lista_notas.append(nota)
+    if "body" not in contenido:
+        texto = ""
+    else:
+        texto = contenido["body"]
     links = []
     # extraemos links
     links = [texto[link.start() + 3:link.start() + 35] for link in re.finditer("\(:", texto)]
     if len(links) == 0:
         continue
-    # los añadimos al diccionario
-    resultado[id] = links # las notas empiezan su numeración en 1, no 0
+    # comprobamos el tipo
+    lista_links_correctos = []
+    for link in links:
+        if todas[link][0] != 1:
+            continue
+        lista_links_correctos.append(link)
+    resultado[nota] = lista_links_correctos
 
 # transformamos todo id en un número
 def id_a_num(id, lista):
+    # debemos comprobar si el id se encuentra en la lista, si eso ocurre
     return lista.index(id)
 resultado = {id_a_num(key, lista_notas): list(map(id_a_num, value, repeat(lista_notas))) for (key, value) in resultado.items()}
 
-# ahora vamos a crear la matriz de adjaciencia
+# ahora vamos a crear la matriz de adyaciencia
 matrix = [[] for nota in lista_notas]
 for row in range(len(lista_notas)):
     # fila a fila
@@ -49,7 +67,7 @@ for row in range(len(lista_notas)):
             # este elemento es 0
             matrix[row].append(0)
 
-# ahora, vamos a transformar la matriz de adjaciencia a formato de texto
+# ahora, vamos a transformar la matriz de adyaciencia a formato de texto
 def list_to_string(lista):
     s = ""
     for i in range(len(lista)):
